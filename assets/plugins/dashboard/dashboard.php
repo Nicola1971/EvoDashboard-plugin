347537fd576ac1030b2e1a5ea3b1eb4d2d374674
Nicola Lambathakis <modxit@modx.it>
2016-10-23 20:47:13 +0200

    new version 3.1

--- assets/plugins/dashboard/dashboard.php
+++ assets/plugins/dashboard/dashboard.php
@@ -1,16 +1,14 @@
 <?php
-/* EvoDashboard 3.0 pl
+/* EvoDashboard 3.1.2 pl
 Instructions:
 System event:
 OnManagerWelcomeHome,OnManagerPageInit,OnManagerMainFrameHeaderHTMLBlock
 Configuration:
-&logobox_showhide=Show Logo Widget:;list;show,hide;show &ShowLogo=Show MODX Logo:;list;show,hide;show &CustomLogo=Show Custom Logo:;list;show,hide;hide &LogoChunk=Logo Chunk:;string;Welcome_YourLogo &userinfo_showhide=Show User Info Widget:;list;show,hide;show &messageinfo_showhide=Show Message Info Widget:;list;show,hide;show &modxnews_showhide=Show ModxNews Widget:;list;show,hide;show &modxsecuritynews_showhide=Show SecurityNews Widget:;list;show,hide;show &recentinfo_showhide=Show Recent Info Widget:;list;show,hide;show &onlineinfo_showhide=Show Online Info Widget:;list;show,hide;show
+&ShowLogo=Show Logo and Title:;list;show,hide;hide;;Show deprecated Dashboard header with logo and site name &LogoPath=Logo Path:;string;../assets/images/logo.png;;set here your custom logo path &welcome_showhide=Show Welcome Widget:;list;show,hide;show &userinfo_showhide=Show User Info inside Welcome widget:;list;show,hide;show &modxwelcome_datarow=welcome widget row position:;string;1;;1-10 &modxwelcome_datacol=welcome widget col position:;string;1;;1-4 &modxwelcome_datasizex=welcome widget x size:;string;2;;1-4 &modxwelcome_datasizey=welcome widget y size:;string;6;;1-10 &onlineinfo_showhide=Show Online Info Widget:;list;show,hide;show &onlineinfo_datarow=onlineinfo widget row position:;string;1;;1-10 &onlineinfo_datacol=onlineinfo widget col position:;string;3;;1-4 &onlineinfo_datasizex=onlineinfo widget x size:;string;2;;1-4 &onlineinfo_datasizey=onlineinfo widget y size:;string;6;;1-4 &recentinfo_showhide=Show Recent Info Widget:;list;show,hide;show &modxrecent_datarow=recent resource widget row position:;string;3;;1-10 &modxrecent_datacol=recent resource widget col position:;string;1;;1-4 &modxrecent_datasizex=recent resource widget x size:;string;4;;1-4 &modxrecent_datasizey=recent resource widget y size:;string;7;;1-10 &modxnews_showhide=Show ModxNews Widget:;list;show,hide;show &modxnews_datarow=modx news widget row position:;string;4;;1-10 &modxnews_datacol=modx news widget col position:;string;1;;1-4 &modxnews_datasizex=modx news widget x size:;string;2;;1-4 &modxnews_datasizey=modx news widget y size:;string;5;;1-10 &modxsecuritynews_showhide=Show SecurityNews Widget:;list;show,hide;show &security_datarow=modx security widget row position:;string;4;;1-10 &security_datacol=modx security widget col position:;string;2;;1-4 &security_datasizex=modx security widget x size:;string;2;;1-4 &security_datasizey=modx security widget y size:;string;5;;1-10
 */
 
 /* home switch home */
 $WelcomeUrl = isset($WelcomeUrl) ? $WelcomeUrl : '../assets/plugins/dashboard/welcome.tpl';
-$StylesUrl = isset($StylesUrl) ? $StylesUrl : '../assets/plugins/dashboard/dashboard.css';
-$cssOutput = isset($cssOutput) ? $cssOutput : '<link type="text/css" rel="stylesheet" href="'.$StylesUrl.'">';
 
 //blocks
 $LogoOutput = isset($LogoOutput) ? $LogoOutput : '';
@@ -24,90 +22,213 @@ $e = &$modx->Event;
 		if($e->name == 'OnManagerPageInit') {
 		$modx->config['manager_welcome_tpl'] = '@FILE: '.$WelcomeUrl.'';
     }
-
+/**
 		if($e->name == 'OnManagerMainFrameHeaderHTMLBlock') {
 		$cssOutput = '
          
         ';
     }
-/*show/hide MODX widgets*/
-if ($logobox_showhide == 'hide') {
-$logobox_display = 'none';
-} else {
-$logobox_display = 'block';
-}
+***/
+//get modx placeholders
+$welcome_title = $modx->getPlaceholder('welcome_title');
 
-$modx->setPlaceholder('logobox_display', $logobox_display);
+$SecurityIcon = $modx->getPlaceholder('SecurityIcon');
+$WebUserIcon = $modx->getPlaceholder('WebUserIcon');
+$ModulesIcon = $modx->getPlaceholder('ModulesIcon');
+$ResourcesIcon = $modx->getPlaceholder('ResourcesIcon');
+$BackupIcon = $modx->getPlaceholder('BackupIcon');
+$HelpIcon = $modx->getPlaceholder('HelpIcon');
 
-/*show/hide MODX widgets*/
+$UserInfo = $modx->getPlaceholder('UserInfo');
+    
+/*show/hide user info inside Welcome widgets*/
+
 if ($userinfo_showhide == 'hide') {
-$userinfo_display = 'none';
+$userinfo_display = '';
 } else {
-$userinfo_display = 'block';
+$userinfo_display = '<div class="userprofiletable">
+              '.$UserInfo.'
+            </div>';
 }
-
 $modx->setPlaceholder('userinfo_display', $userinfo_display);
 
-/*show/hide MODX widgets*/
-if ($messageinfo_showhide == 'hide') {
-$messageinfo_display = 'none';
+/*show/hide MODX welcome widgets*/
+if ($welcome_showhide == 'hide') {
+$welcome_display = '';
 } else {
-$messageinfo_display = 'block';
+$welcome_display = '<!---Welcome Logo and buttons---> 
+      <!--- panel -->
+      <li id="modxwelcome_widget" data-row="'.$modxwelcome_datarow.'" data-col="'.$modxwelcome_datacol.'" data-sizex="'.$modxwelcome_datasizex.'" data-sizey="'.$modxwelcome_datasizey.'">
+        <div class="panel panel-default widget-wrapper">
+          <div class="panel-headingx widget-title sectionHeader clearfix">
+            <span class="panel-handel pull-left"><i class="fa fa-home"></i> '.$welcome_title.'</span>
+            <div class="widget-controls pull-right">
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="modxwelcome_widget"></a>
+              </div>    
+            </div>
+          </div>
+          <div class="panel-body widget-stage sectionBody">
+            <div class="wm_buttons">
+              '.$SecurityIcon.'
+              '.$WebUserIcon.'
+              '.$ModulesIcon.'
+              '.$ResourcesIcon.'
+              '.$BackupIcon.'
+              '.$HelpIcon.'
+            </div>
+              '.$userinfo_display .'
+          </div>
+        </div>      
+      </li>
+      <!--- /panel --->';
 }
 
-$modx->setPlaceholder('messageinfo_display', $messageinfo_display);
+$modx->setPlaceholder('welcome_display', $welcome_display);
 
-/*show/hide MODX widgets*/
-if ($modxnews_showhide == 'hide') {
-$modxnews_display = 'none';
+
+/*show/hide MODX online users widgets*/
+//get modx placeholders
+$onlineusers_title = $modx->getPlaceholder('onlineusers_title');
+$OnlineInfo = $modx->getPlaceholder('OnlineInfo');
+
+if ($onlineinfo_showhide == 'hide') {
+$onlineinfo_display = 'none';
 } else {
-$modxnews_display = 'block';
+$onlineinfo_display = '<!---User Info--->
+      <!--- panel --->
+      <li id="modxonline_widget" data-row="'.$onlineinfo_datarow.'" data-col="'.$onlineinfo_datacol.'" data-sizex="'.$onlineinfo_datasizex.'" data-sizey="'.$onlineinfo_datasizey.'">
+        <div class="panel panel-default widget-wrapper">
+          <div class="panel-headingx widget-title sectionHeader clearfix">
+            <span class="panel-handel pull-left"><i class="fa fa-user"></i> '.$onlineusers_title.'</span>
+            <div class="widget-controls pull-right">
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="modxonline_widget"></a>
+              </div>    
+            </div>
+          </div>
+          <div class="panel-body widget-stage sectionBody">
+            <div class="userstable">
+              '.$OnlineInfo.'
+            </div>
+          </div>
+        </div>      
+      </li>
+      <!--- /panel --->';
 }
+$modx->setPlaceholder('onlineinfo_display', $onlineinfo_display);
 
-$modx->setPlaceholder('modxnews_display', $modxnews_display);
 
-/*show/hide MODX widgets*/
-if ($modxsecuritynews_showhide == 'hide') {
-$modxsecuritynews_display = 'none';
-} else {
-$modxsecuritynews_display = 'block';
-}
+/*show/hide Recent doc MODX widgets*/
+//get modx placeholders
+$activity_title = $modx->getPlaceholder('activity_title');
+$RecentInfo = $modx->getPlaceholder('RecentInfo');
 
-$modx->setPlaceholder('modxsecuritynews_display', $modxsecuritynews_display);
-
-/*show/hide MODX widgets*/
 if ($recentinfo_showhide == 'hide') {
-$recentinfo_display = 'none';
+$recentinfo_display = '';
 } else {
-$recentinfo_display = 'block';
+$recentinfo_display = ' <!---Recent Resources--->
+      <!--- panel --->
+      <li id="modxrecent_widget" data-row="'.$modxrecent_datarow.'" data-col="'.$modxrecent_datacol.'" data-sizex="'.$modxrecent_datasizex.'" data-sizey="'.$modxrecent_datasizey.'">
+        <div class="panel panel-default widget-wrapper">
+          <div class="panel-headingx widget-title sectionHeader clearfix">
+            <span class="panel-handel pull-left"><i class="fa fa-pencil-square-o"></i> '.$activity_title.'</span>
+            <div class="widget-controls pull-right">
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="modxrecent_widget"></a>
+              </div>    
+            </div>
+          </div>
+          <div class="panel-body widget-stage sectionBody">
+            '.$RecentInfo.'
+          </div>
+        </div>      
+      </li>
+      <!--- /panel --->     ';
 }
 
 $modx->setPlaceholder('recentinfo_display', $recentinfo_display);
 
-/*show/hide MODX widgets*/
-if ($onlineinfo_showhide == 'hide') {
-$onlineinfo_display = 'none';
+/*show/hide MODX News widgets*/
+//get modx placeholders
+$modx_news_title = $modx->getPlaceholder('modx_news_title');
+$modx_news_content = $modx->getPlaceholder('modx_news_content');
+
+if ($modxnews_showhide == 'hide') {
+$modxnews_display = '';
 } else {
-$onlineinfo_display = 'block';
+$modxnews_display = '<!---MODX News--->
+      <!--- panel --->
+      <li id="modxnews_widget" data-row="'.$modxnews_datarow.'" data-col="'.$modxnews_datacol.'" data-sizex="'.$modxnews_datasizex.'" data-sizey="'.$modxnews_datasizey.'">
+        <div class="panel panel-default widget-wrapper">
+          <div class="panel-headingx widget-title sectionHeader clearfix">
+            <span class="panel-handel pull-left"><i class="fa fa-rss"></i> '.$modx_news_title.'</span>
+            <div class="widget-controls pull-right">
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="modxnews_widget"></a>
+              </div>    
+            </div>
+          </div>
+          <div class="panel-body widget-stage sectionBody">
+            '.$modx_news_content.'
+          </div>
+        </div>      
+      </li>
+      <!--- /panel --->';
 }
 
-$modx->setPlaceholder('onlineinfo_display', $onlineinfo_display);
+$modx->setPlaceholder('modxnews_display', $modxnews_display);
+
+/*show/hide MODX security widgets*/
+//get modx placeholders
+$modx_security_notices_title = $modx->getPlaceholder('modx_security_notices_title');
+$modx_security_notices_content = $modx->getPlaceholder('modx_security_notices_content');
+if ($modxsecuritynews_showhide == 'hide') {
+$modxsecuritynews_display = '';
+} else {
+$modxsecuritynews_display = '<!---Security News--->
+      <!--- panel --->
+      <li id="modxsecurity_widget" data-row="'.$security_datarow.'" data-col="'.$security_datacol.'" data-sizex="'.$security_datasizex.'" data-sizey="'.$security_datasizey.'">
+        <div class="panel panel-default widget-wrapper">
+          <div class="panel-headingx widget-title sectionHeader clearfix">
+            <span class="panel-handel pull-left"><i class="fa fa-exclamation-triangle"></i> '.$modx_security_notices_title.'</span>
+            <div class="widget-controls pull-right">
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="modxsecurity_widget"></a>
+              </div>    
+            </div>
+          </div>
+          <div class="panel-body widget-stage sectionBody">
+            '.$modx_security_notices_content.'
+          </div>
+        </div>      
+      </li>
+      <!--- /panel --->
+';
+}
+
+$modx->setPlaceholder('modxsecuritynews_display', $modxsecuritynews_display);
+
 /*Custom logo */
 
 //logo
-
-if ($CustomLogo == 'show') {
-$LogoOutput = $modx->getChunk(''.$LogoChunk.'');
-$LogoButton = '<span class="wm_button white" style="border:0">'.$LogoOutput.'</span>';
-}
-else {
 if ($ShowLogo == 'show') {
-$LogoButton = ' <span class="wm_button white" style="border:0"><img src="media/style/' . $modx->config['manager_theme'] . '/images/misc/logo.png" alt="[+logo_slogan+]" /></span>';
+$LogoButton = '   <!-- title-->
+  <div class="dashboard_header">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="wm_logo">
+          <img src="'.$LogoPath.'" alt="[+logo_slogan+]" />
+        </div>
+        <h1>[+site_name+]</h1>
+      </div>
+    </div>
+  </div>';
 }
 else {
 $LogoButton = '';    
 }
-}
+
 $modx->setPlaceholder('Logo', $LogoButton);
 
 //end logo
--- assets/plugins/dashboard/welcome.tpl
+++ assets/plugins/dashboard/welcome.tpl
@@ -1,221 +1,66 @@
-        <link rel="stylesheet" type="text/css" href="../assets/plugins/dashboard/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="../assets/plugins/dashboard/bootstrap/css/bootstrap-theme.css" />
-        <link rel="stylesheet" href="../assets/plugins/dashboard/plugins/gridster/jquery.gridster.css" />
-        <link rel="stylesheet" href="../assets/plugins/dashboard/css/gridster-dashboard.css" />
+<link rel="stylesheet" type="text/css" href="media/style/common/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="media/script/gridster/jquery.gridster.css" />
+<link rel="stylesheet" href="../assets/plugins/dashboard/dashboard/css/dashboard.css" />
+<link rel="stylesheet" href="../assets/plugins/dashboard/dashboard/css/colors.css" />
+<link rel="stylesheet" href="media/style/common/font-awesome/css/font-awesome.min.css" />
 
 <div class="container-fluid dashboard">
 
-      <!-- title-->
 
-     <div class="col-sm-12">
-     [+OnManagerWelcomePrerender+]
-    <h1>[+site_name+] / Dashboard</h1>
-     </div>
-     <!-- alert -->
-     <div class="container col-sm-12" style="display:[+config_display+]">
-           <div class="widget-wrapper alert alert-warning">
-             [+config_check_results+]
-          </div>
+  [+OnManagerWelcomePrerender+]
+ [+Logo+]
+  <!-- alert -->
+  <div style="display:[+config_display+]">
+    <div class="widget-wrapper alert alert-warning">
+      [+config_check_results+]
     </div>
- </div>
-
-       <!-- end  title -->
-<div class="container-fluid dashboard">
-       <div class="col-sm-12">
- <!-- GridSter widgets -->
-                <div class="gridster margin-bottom-30">
-                  
-                     <ul>
-                    [+OnManagerWelcomeHome+]
-                  <!---Welcome Logo and buttons---> 
-                  <!--- panel -->
-                  				<li id="modxwelcome_widget" data-row="1" data-col="1" data-sizex="3" data-sizey="3" style="display:[+logobox_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-home"></i> [+welcome_title+]</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxwelcome_widget"></a>
-								</div>	  
-							</div>
-
-					  </div>
-					  <div class="panel-body widget-stage sectionBody">
-                                  <div class="wm_logo">
-                 [+Logo+]
-              </div>
-	            
-                       <div class="wm_button red" style="border:0">[+SecurityIcon+]</div>
-                        <div class="wm_button green" style="border:0">[+WebUserIcon+]</div>
-                        <div class="wm_button azure" style="border:0">[+ModulesIcon+]</div>
-                        <div class="wm_button yellow" style="border:0">[+ResourcesIcon+]</div>
-                        <div class="wm_button violet" style="border:0">[+BackupIcon+]</div>
-                       <!--- <div class="wm_button orange" style="border:0">[+HelpIcon+]</div>---> 
-                       
-					  </div>
-					</div>			
-				</li>
-                  <!--- /panel --->
-                          
-   
-                   
-                   <!---Messages--->
-                    <!--- panel --->
-                   <li id="modxinbox_widget" data-row="1" data-col="2" data-sizex="1" data-sizey="3" style="display:[+messageinfo_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-inbox"></i> Inbox</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxinbox_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-							<ul class="list-group">
-							[+MessageInfo+]
-							</ul>
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                   
-                 <!---Online Users--->
-                                  <!--- panel --->
-                   <li id="modxonlineusers_widget" data-row="2" data-col="1" data-sizex="2" data-sizey="5" style="display:[+onlineinfo_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-users"></i> [+onlineusers_title+]</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxonlineusers_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-							<ul class="list-group">
-							[+OnlineInfo+]
-							</ul>
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                    <!---User Info--->
-                                                  <!--- panel --->
-                   <li id="modxinfo_widget" data-row="2" data-col="2" data-sizex="2" data-sizey="5" style="display:[+userinfo_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-info-circle"></i> Info</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxinfo_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-						<i class="fa fa-info-circle fa-5x  icon-color-verylight"></i>	   [+UserInfo+]
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                          
-                    
-                    <!---Recent Resources--->
-                   <!--- panel --->
-                   <li id="modxrecent_widget" data-row="3" data-col="1" data-sizex="4" data-sizey="5" style="display:[+recentinfo_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-pencil-square-o"></i> [+activity_title+]</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxrecent_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-							    [+RecentInfo+]
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                     
-                    
-                    <!---MODX News--->
-                  <!--- panel --->
-                   <li id="modxnews_widget" data-row="4" data-col="1" data-sizex="2" data-sizey="5" style="display:[+modxnews_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-rss"></i> [+modx_news_title+]</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxnews_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-						 <i class="fa fa-rss fa-5x icon-color-verylight"></i> [+modx_news_content+]
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                    
-                    <!---Security News--->
-                 <!--- panel --->
-                   <li id="modxsecurity_widget" data-row="4" data-col="2" data-sizex="2" data-sizey="5" style="display:[+modxsecuritynews_display+]">
-					<div class="panel panel-default widget-wrapper">
-					  <div class="panel-headingx widget-title sectionHeader clearfix">
-						  <span class="panel-handel pull-left"><i class="fa fa-exclamation-triangle"></i> [+modx_security_notices_title+]</span>
-							<div class="widget-controls pull-right">
-								<div class="btn-group">
-									<a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="modxsecurity_widget"></a>
-								</div>	  
-							</div>
-					  </div>
-                        <div class="panel-body widget-stage sectionBody">
-						<i class="fa fa-exclamation-triangle fa-5x icon-color-verylight"></i> [+modx_security_notices_content+]
-					  </div>
-					</div>			
-				</li>
-                 <!--- /panel --->
-                                  
-                </div>
-                <!-- / GridStack widgets -->
- </div>
-<div class="container-fluid dashboard">
- [+OnManagerWelcomeRender+] 
-      <!-- row title-->
-    <div class="row">
-     <div class="container col-sm-12 margin-bottom-30">
- 
-     <p class=" text-muted pull-right">
-<a class="btn btn-sm btn-default" onclick="cleanLocalStorage('[+site_name+]-evodashboard.grid,[+site_name+]-evodashboard.states')"><i class="fa fa-refresh" aria-hidden="true"></i> Reset Grid</a>
-</p>
-     <p class=" text-muted pull-left">
-EvoDashboard 3.0
-</p>
- </div>
   </div>
-    </div>
 
-<script src="../assets/plugins/dashboard/plugins/jquery-2.1.4.min.js"></script>
-<script src='../assets/plugins/dashboard/plugins/gridster/jquery.gridster.js'></script>
-<script src='../assets/plugins/dashboard/js/evodashboard.js'></script>
-<script src="../assets/plugins/dashboard/bootstrap/js/bootstrap.min.js"></script>
-		<script type="text/javascript">
-			//localStorage.clear();
+</div>
 
-			var localdata_position = JSON.parse(localStorage.getItem('[+site_name+]-evodashboard.grid'));
-			var localdata_states = JSON.parse(localStorage.getItem('[+site_name+]-evodashboard.states'));
+<!-- end  title -->
+<div class="dashboard">
+  <!-- GridSter widgets -->
+  <div class="gridster">
+    <ul>
+
+[+OnManagerWelcomeHome+]      
+[+welcome_display+]      
+[+onlineinfo_display+]
+[+recentinfo_display+]
+[+modxnews_display+]
+[+modxsecuritynews_display+]
+    </ul>           
+  </div>
+  <!-- / GridStack widgets -->                    
+  [+OnManagerWelcomeRender+] 
+  <div class="container-fluid">
+    <p class="text-muted pull-right">
+      <a class="btn btn-sm btn-default" onclick="cleanLocalStorage('[+site_name+]-evodashboard.grid,[+site_name+]-evodashboard.states')"><i class="fa fa-refresh" aria-hidden="true"></i> [+resetgrid+]</a>
+    </p>
+  </div>
+
+</div>
+
+<script src="media/script/jquery/jquery.min.js"></script>
+<script src='media/script/gridster/jquery.gridster.min.js'></script>
+<script src='../assets/plugins/dashboard/dashboard/js/evodashboard.js'></script>
+<script src="media/script/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+  //localStorage.clear();
+
+  var localdata_position = JSON.parse(localStorage.getItem('[+site_name+]-evodashboard.grid'));
+  var localdata_states = JSON.parse(localStorage.getItem('[+site_name+]-evodashboard.states'));
 
 
-			fnCreateGridster('[+site_name+]-evodashboard.grid', '[+site_name+]-evodashboard.states');
-		</script>
-        
-		<script type="text/javascript">        
-        function cleanLocalStorage() {
+  fnCreateGridster('[+site_name+]-evodashboard.grid', '[+site_name+]-evodashboard.states');
+</script>
+
+<script type="text/javascript">        
+  function cleanLocalStorage() {
     for(key in localStorage) {
-        delete localStorage[key];
+      delete localStorage[key];
     }
     location.reload();
-}
- </script>
+  }
+</script>
